004b5f490bf976d00c459d03727a4c0565916000
Simon Moreau <simon@bim42.com>
2016-02-24 18:29:29 +0100

    First Commit
    First Commit

---
+++ GoogleGeocoding/GeocodingResponse.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace GoogleGeocoding
+{
+
+    public class GeocodingResponses
+    {
+        public List<GeocodingResponse> results { get; set; }
+        public string status { get; set; }
+
+        public GeocodingResponses() {}
+
+        public GeocodingResponses(string address)
+        {
+
+            string requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/json?address={0}&sensor=false", Uri.EscapeDataString(address));
+
+            WebRequest request = WebRequest.Create(requestUri);
+            WebResponse response = request.GetResponse();
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                string json = sr.ReadToEnd();
+                GeocodingResponses responses = JsonConvert.DeserializeObject<GeocodingResponses>(json);
+                this.results = responses.results;
+                this.status = responses.status;
+            }
+        }
+    }
+
+    public class GeocodingResponse
+    {
+        public List<AddressComponent> address_components { get; set; }
+        public string formatted_address { get; set; }
+        public Geometry geometry { get; set; }
+        public string place_id { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class AddressComponent
+    {
+        public string long_name { get; set; }
+        public string short_name { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class Geometry
+    {
+        public Location location { get; set; }
+        public string location_type { get; set; }
+        public Viewport viewport { get; set; }
+    }
+
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+    public class Northeast
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Southwest
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+}
---
+++ GoogleGeocoding/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Diagnostics;
+using edu.stanford.nlp.ie.crf;
+
+namespace GoogleGeocoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string addsFilePath = @"C:\Users\moreaus\Desktop\Dev\adds.csv";
+            string[] lines = File.ReadAllLines(addsFilePath, Encoding.GetEncoding("iso-8859-1"));
+
+            // Path to the folder with classifiers models
+            string jarRoot = @"C:\Affaires\01-Developpement\GoogleGeocoding\stanford-ner-2015-12-09";
+            string classifiersDirecrory = jarRoot + @"\classifiers";
+
+            // Loading 3 class classifier model
+            CRFClassifier classifier = CRFClassifier.getClassifierNoExceptions(classifiersDirecrory + @"\english.all.3class.distsim.crf.ser.gz");
+
+            foreach (string line in lines)
+            {
+                string description = line.Split(';')[1];
+
+                var classified = classifier.classifyToCharacterOffsets(description).toArray();
+
+                //for (int i = 0; i < classified.Length; i++)
+                //{
+
+                //}
+
+                //var s1 = "Good afternoon Rajat Raina, how are you today?";
+                //Debug.WriteLine("{0}\n", classifier.classifyToString(s1));
+
+                //var s2 = "I go to school at Stanford University, which is located in California.";
+                //Debug.WriteLine("{0}\n", classifier.classifyWithInlineXML(s2));
+
+                //Debug.WriteLine("{0}\n", classifier.classifyToString(s2, "xml", true));
+
+                string zipCode = line.Split(';')[4];
+                //GeocodingResponses responses = new GeocodingResponses(zipCode + " - " + Description);
+                //if (responses.results.Count != 0)
+                //{
+                //    Debug.WriteLine(zipCode + " - " + Description + " _ " + responses.status + "_" + responses.results.FirstOrDefault().formatted_address);
+                //}
+                //else
+                //{
+                //    Debug.WriteLine(responses.status);
+                //}
+                
+                //System.Threading.Thread.Sleep(100);
+            } 
+           // string address = "123 something st, somewhere";
+            
+        }
+
+    }
+}
